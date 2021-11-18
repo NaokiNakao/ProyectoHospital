3504d2b50ed7497977fa65ae62d2ce948b6b23be
Naoki Nakao <naokinakao.nn@gmail.com>
2021-11-18 14:08:43 -0800

    Codigo HistoriaClinica

--- Proyecto Final/src/logico/Historiaclinica.java
+++ Proyecto Final/src/logico/Historiaclinica.java
@@ -1,9 +1,19 @@
 package logico;
 
 public class Historiaclinica {
+	
+	private String codigo;
 
 	public Historiaclinica() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
 	}
 
 }
