01cbc402c5ffb9135f66de07a856f822ea7f3da6
migu1 <migu1@LAPTOP-R967STKN>
2021-11-18 15:29:15 -0400

    Codigo Enfermedad

--- Proyecto Final/src/logico/Enfermedad.java
+++ Proyecto Final/src/logico/Enfermedad.java
@@ -2,8 +2,37 @@ package logico;
 
 public class Enfermedad {
 
+	private String tipoenfermedad;
+	private String nombenferme;
+	private String codienferme;
+ 
 	public Enfermedad() {
 		// TODO Auto-generated constructor stub
 	}
+
+	public String getTipoenfermedad() {
+		return tipoenfermedad;
+	}
+
+	public void setTipoenfermedad(String tipoenfermedad) {
+		this.tipoenfermedad = tipoenfermedad;
+	}
+
+	public String getNombenferme() {
+		return nombenferme;
+	}
+
+	public void setNombenferme(String nombenferme) {
+		this.nombenferme = nombenferme;
+	}
+
+	public String getCodienferme() {
+		return codienferme;
+	}
+
+	public void setCodienferme(String codienferme) {
+		this.codienferme = codienferme;
+	}
+	
 
 }
