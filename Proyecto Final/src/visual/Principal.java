71832161d032312233ac87aad57418174d01959b
Admin <Admin@DESKTOP-4641GKN>
2022-08-03 12:31:45 -0400

    Quitado logica de ficheros ventana Principal

--- Proyecto Final/src/visual/Principal.java
+++ Proyecto Final/src/visual/Principal.java
@@ -6,27 +6,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import logico.Administrador;
-import logico.Clinica;
-import logico.Servidor;
-import logico.Usuario;
-
 import javax.swing.JLabel;
-import javax.imageio.IIOException;
 import javax.swing.ImageIcon;
 import java.awt.Toolkit;
 import java.awt.Font;
 import java.awt.event.ActionListener;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import java.awt.event.WindowAdapter;
@@ -40,7 +24,6 @@ public class Principal extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private Dimension dim;
-	private Socket sesion;
 
 	/**
 	 * Launch the application.
@@ -48,41 +31,6 @@ public class Principal extends JFrame {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				FileInputStream respaldoIn;
-				FileOutputStream respaldoOut;
-				ObjectInputStream respaldoRead;
-				ObjectOutputStream respaldoWrite;
-				
-				try {
-					respaldoIn = new FileInputStream(Servidor.respaldo);
-					respaldoRead = new ObjectInputStream(respaldoIn);
-					Clinica temp = (Clinica) respaldoRead.readObject();
-					Clinica.setClinica(temp);
-					respaldoIn.close();
-					respaldoRead.close();
-				} catch (FileNotFoundException e) {
-					try {
-						respaldoOut = new FileOutputStream(Servidor.respaldo);
-						respaldoWrite = new ObjectOutputStream(respaldoOut);
-						Administrador aux = new Administrador(Clinica.getInstance().generadorCodigo(8), "admin", "admin", "", "", "", "");
-						Clinica.getInstance().registroUsuario(aux);
-						respaldoWrite.writeObject(Clinica.getInstance());
-						respaldoOut.close();
-						respaldoWrite.close();
-					} catch (FileNotFoundException e1) {
-						
-					}
-					catch (IOException e1) {
-						
-					}
-				}
-				catch (IOException e) {
-					
-				}
-				catch (ClassNotFoundException e) {
-					e.printStackTrace();
-				}
-				
 				try {
 					Principal frame = new Principal();
 					frame.setVisible(true);
@@ -100,7 +48,7 @@ public class Principal extends JFrame {
 		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent arg0) {
-				respaldo();
+				
 			}
 		});
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Principal.class.getResource("/pictures/istockphoto-1130389312-612x612.jpg")));
@@ -138,7 +86,6 @@ public class Principal extends JFrame {
 		JButton btnCancelar = new JButton("Salir");
 		btnCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				respaldo();
 				dispose();
 			}
 		});
@@ -150,23 +97,6 @@ public class Principal extends JFrame {
 		lblHospitalJuanJose.setBounds(488, 56, 600, 43);
 		contentPane.add(lblHospitalJuanJose);
 		setLocationRelativeTo(null);
-	}
-	
-
-	
-	private void respaldo() {
-		String serverAddr = Servidor.addr;
-		int serverPort = Servidor.port;
-		
-		try {
-			sesion = new Socket(serverAddr, serverPort);
-			ObjectOutputStream salida = new ObjectOutputStream(sesion.getOutputStream());
-			salida.writeObject(Clinica.getInstance());
-			sesion.close();
-		} catch (IOException ioe) {
-			System.out.println("No se puede conectar al servidor.");
-			System.exit(1);
-		}
 	}
 	
 }
