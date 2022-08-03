ba0c683844b5a907ae09ee82aa707e33c3898572
Admin <Admin@DESKTOP-4641GKN>
2022-08-03 09:49:13 -0400

    Formato clase ConexionSQL

--- Proyecto Final/src/logico/ConexionSQL.java
+++ Proyecto Final/src/logico/ConexionSQL.java
@@ -7,25 +7,24 @@ import java.sql.SQLException;
 public class ConexionSQL {
 	
 	
-public static Connection getConexion() {
-		
-        String Conurl = "jdbc:sqlserver://192.168.100.118:1433;" 
-    + "database=ventas_Grupo_2;"
-    + "user=mvasquez;"
-    + "password=Eict@2022;"
-    + "loginTimeout=30;"
-    + "encrypt=true;"
-    + "trustServerCertificate=true;";
-        
-		try {
+	public static Connection getConexion() {
 			
-			Connection con = DriverManager.getConnection(Conurl);
-			return con;
+	        String Conurl = "jdbc:sqlserver://192.168.100.118:1433;" 
+	        				+ "database=ventas_Grupo_2;"
+	        				+ "user=mvasquez;"
+	        				+ "password=Eict@2022;"
+	        				+ "loginTimeout=30;"
+	        				+ "encrypt=true;"
+	        				+ "trustServerCertificate=true;";
+	        
+			try {
+				Connection con = DriverManager.getConnection(Conurl);
+				return con;
+			}catch(SQLException ex) {
+				System.out.println(ex.toString());
+				return null;
+			}	
 			
-		}catch(SQLException ex) {
-			System.out.println(ex.toString());
-			return null;
-		}	
 	}
 
 }
