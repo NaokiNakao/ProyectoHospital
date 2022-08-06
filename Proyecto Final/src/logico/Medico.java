46e918652b9726396c5a909558e4a63d6eb3595b
Admin <Admin@DESKTOP-4641GKN>
2022-08-06 14:04:58 -0400

    Quitado atributo "especialidad"

--- Proyecto Final/src/logico/Medico.java
+++ Proyecto Final/src/logico/Medico.java
@@ -4,24 +4,24 @@ import java.util.ArrayList;
 
 public class Medico extends Usuario {
 
-	private String especialidad;
+	//private String especialidad;
 	private ArrayList<CitaMedica> misCitas;
 	private ArrayList<Consulta> misConsultas;
 
 	public Medico(String id, String login, String password, String nombre, String apellido, String telefono, String especialidad) {
 		super(id, login, password, nombre, apellido, telefono);
-		this.especialidad = especialidad;
+		//this.especialidad = especialidad;
 		misCitas = new ArrayList<CitaMedica>();
 		misConsultas = new  ArrayList<Consulta>();
 	}
 
-	public String getEspecialidad() {
+	/*public String getEspecialidad() {
 		return especialidad;
 	}
 
 	public void setEspecialidad(String especialidad) {
 		this.especialidad = especialidad;
-	}
+	}*/
 
 	public ArrayList<CitaMedica> getMisCitas() {
 		return misCitas;
