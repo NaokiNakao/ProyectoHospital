6306988844b2823de4bc2f293c36e7f5da26fa1b
Admin <Admin@DESKTOP-4641GKN>
2022-08-07 00:25:59 -0400

    Modificacion CitaMedica

--- Proyecto Final/src/logico/CitaMedica.java
+++ Proyecto Final/src/logico/CitaMedica.java
@@ -6,29 +6,30 @@ public class CitaMedica {
 
 	private String codigo;
 	private Date fechaCita;
-	private String nombrePersona;
-	private String telefono;
 	private Medico medico;
-	private String cedulaPersona;
-	private String generoPersona;
-	private Date fechaNacimientoPersona;
-	private String direccionPersona;
-	private HistoriaClinica historialPersona;
-	private Date fechaN;
+	private Paciente paciente;
+	//private String nombrePersona;
+	//private String telefono;
+	//private String cedulaPersona;
+	//private String generoPersona;
+	//private Date fechaNacimientoPersona;
+	//private String direccionPersona;
+	//private HistoriaClinica historialPersona;
+	//private Date fechaN;
 	
-	public CitaMedica(String codigo, Date fechaCita, String nombrePersona, String telefono, Medico medico,
-			String cedulaPersona, String generoPersona, Date fechaNacimientoPersona, String direccionPersona, Date fechaN) {
-		
+	
+	public CitaMedica(String codigo, Date fechaCita, Medico medico, Paciente paciente) {
 		this.codigo = codigo;
 		this.fechaCita = fechaCita;
-		this.nombrePersona = nombrePersona;
-		this.telefono = telefono;
 		this.medico = medico;
-		this.cedulaPersona = cedulaPersona;
-		this.generoPersona = generoPersona;
-		this.fechaNacimientoPersona = fechaNacimientoPersona;
-		this.direccionPersona = direccionPersona;
-		this.fechaN = fechaN;
+		this.paciente = paciente;
+		//this.nombrePersona = nombrePersona;
+		//this.telefono = telefono;
+		//this.cedulaPersona = cedulaPersona;
+		//this.generoPersona = generoPersona;
+		//this.fechaNacimientoPersona = fechaNacimientoPersona;
+		//this.direccionPersona = direccionPersona;
+		//this.fechaN = fechaN;
 	}
 
 	public String getCodigo() {
@@ -38,18 +39,22 @@ public class CitaMedica {
 	public Date getFechaCita() {
 		return fechaCita;
 	}
-
-	public String getNombrePersona() {
-		return nombrePersona;
-	}
-
-	public String getTelefono() {
-		return telefono;
-	}
-
+	
 	public Medico getMedico() {
 		return medico;
 	}
+	
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	/*
+	 * public String getNombrePersona() { return nombrePersona; }
+	 */
+
+	/*
+	 * public String getTelefono() { return telefono; }
+	 */
 
 	public void setCodigo(String codigo) {
 		this.codigo = codigo;
@@ -58,65 +63,49 @@ public class CitaMedica {
 	public void setFechaCita(Date fechaCita) {
 		this.fechaCita = fechaCita;
 	}
-
-	public void setNombrePersona(String nombrePersona) {
-		this.nombrePersona = nombrePersona;
-	}
-
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-
+	
 	public void setMedico(Medico medico) {
 		this.medico = medico;
 	}
-
-	public String getCedulaPersona() {
-		return cedulaPersona;
+	
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
 	}
 
-	public void setCedulaPersona(String cedulaPersona) {
-		this.cedulaPersona = cedulaPersona;
-	}
-
-	public String getGeneroPersona() {
-		return generoPersona;
-	}
-
-	public void setGeneroPersona(String generoPersona) {
-		this.generoPersona = generoPersona;
-	}
-
-	public Date getFechaNacimientoPersona() {
-		return fechaNacimientoPersona;
-	}
-
-	public void setFechaNacimientoPersona(Date fechaNacimientoPersona) {
-		this.fechaNacimientoPersona = fechaNacimientoPersona;
-	}
-
-	public String getDireccionPersona() {
-		return direccionPersona;
-	}
-
-	public void setDireccionPersona(String direccionPersona) {
-		this.direccionPersona = direccionPersona;
-	}
-
-	public HistoriaClinica getHistorialPersona() {
-		return historialPersona;
-	}
-
-	public void setHistorialPersona(HistoriaClinica historialPersona) {
-		this.historialPersona = historialPersona;
-	}
-
-	public Date getFechaN() {
-		return fechaN;
-	}
-
-	public void setFechaN(Date fechaN) {
-		this.fechaN = fechaN;
-	}
+	/*
+	 * public void setNombrePersona(String nombrePersona) { this.nombrePersona =
+	 * nombrePersona; }
+	 * 
+	 * public void setTelefono(String telefono) { this.telefono = telefono; }
+	 * 
+	 * public String getCedulaPersona() { return cedulaPersona; }
+	 * 
+	 * public void setCedulaPersona(String cedulaPersona) { this.cedulaPersona =
+	 * cedulaPersona; }
+	 * 
+	 * public String getGeneroPersona() { return generoPersona; }
+	 * 
+	 * public void setGeneroPersona(String generoPersona) { this.generoPersona =
+	 * generoPersona; }
+	 * 
+	 * public Date getFechaNacimientoPersona() { return fechaNacimientoPersona; }
+	 * 
+	 * public void setFechaNacimientoPersona(Date fechaNacimientoPersona) {
+	 * this.fechaNacimientoPersona = fechaNacimientoPersona; }
+	 * 
+	 * public String getDireccionPersona() { return direccionPersona; }
+	 * 
+	 * public void setDireccionPersona(String direccionPersona) {
+	 * this.direccionPersona = direccionPersona; }
+	 * 
+	 * public HistoriaClinica getHistorialPersona() { return historialPersona; }
+	 * 
+	 * public void setHistorialPersona(HistoriaClinica historialPersona) {
+	 * this.historialPersona = historialPersona; }
+	 * 
+	 * public Date getFechaN() { return fechaN; }
+	 * 
+	 * public void setFechaN(Date fechaN) { this.fechaN = fechaN; }
+	 */
 	
 }
